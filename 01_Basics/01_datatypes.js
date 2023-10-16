74b06a8ca30662547dcb9528571006ede67ff759
KrishCode123 <148032783+KrishCode123@users.noreply.github.com>
2023-10-16 05:48:56 +0000

    datatypes

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all the js code as newer version 
+
+
+let name="Krishnan"
+let age=20
+let caste=null
+let state;
+let gf=true
+
+console.table([typeof(name), typeof(age), typeof(caste), typeof(state)])
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=2145
+var accountEmail="krish@k.com"
+let accountPhone="125468974"
+
+accountAdress="Jaipur"
+let accountState
+
+// changing the accountId
+// accountId=2 Can't be changed
+
+accountEmail="abs@sv"
+accountPhone="98547524572"
+accountAdress="Pune"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPhone, accountAdress, accountState])
