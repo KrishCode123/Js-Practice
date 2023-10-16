7d5232a63f34e69ca1802d5c0781deb91ff17814
KrishCode123 <148032783+KrishCode123@users.noreply.github.com>
2023-10-16 16:04:49 +0000

    Converssion

---
+++ 01_Basics/03_convop.js
@@ -0,0 +1,14 @@
+let score= "33abc"
+
+console.log(score);
+console.log(typeof(score));
+
+let v2i = Number(score)
+console.log(typeof(v2i));
+console.log(v2i);
+
+let some= 1
+
+let op = Boolean(some)
+console.log(op);
+console.log(typeof(op));
