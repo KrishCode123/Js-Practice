6c24b948a094a608fb9b7d8e8431b664d9a91849
KrishCode123 <148032783+KrishCode123@users.noreply.github.com>
2023-10-17 05:01:57 +0000

    comp dome

---
+++ 01_Basics/05_comp.js
@@ -0,0 +1,6 @@
+console.table([2>2, 2>1, 1==1, 1!=1, 0!=6, 6>1])
+console.table([null==0,null>0,null>=0])
+console.table([undefined==0, undefined>0, undefined >=0])
+console.table(["2">=0, "2"==0])
+console.log("2"===1);
+
